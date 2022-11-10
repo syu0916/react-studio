caa75d5974d018eef4eaa6a76e25ab8b6067ce4b
SeanYu <sean_yu@brown.edu>
2022-11-09 22:47:43 -0500

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
+
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
@@ -9,6 +10,17 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
+  const[cart, cartList] = useState([]);
+  const[total, keepTrack] = useState(0);
+
+  function handleList(prop) {
+    cartList(cart => [prop, " ", ...cart]);
+    console.log(cart);
+  }
+
+  function totalPrice(prop){
+    keepTrack(total + prop);
+  }
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
@@ -16,16 +28,36 @@ function App() {
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+      <div className = "items">
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        BakeryItem(item, handleList, totalPrice)
       ))}
+
+      </div>
+      
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <p> Cart Contents: {cart}</p>
+        <p> here is the total: {total}</p>
       </div>
     </div>
   );
+}
+
+function BakeryItem(item, handleList, totalPrice){
+  return (
+    <div className = "bakery-item">
+      <h2>{item.name} </h2>
+      <img src = {item.image}/> 
+      <button type="button" class="btn btn-success" 
+        onClick = {() => {handleList(item.name); totalPrice(item.price)}}>Click to add: {item.price}
+      </button>
+      
+      
+    </div>
+  );
+      
 }
 
 export default App;
